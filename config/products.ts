584dcdc07ff50e6b9b6c9023be28036c468d366e
Kim-Bao Le <kim-bao.le@ekino.com>
2023-06-16 17:52:05 +0700

    feat: add test create product

---
+++ config/products.ts
@@ -0,0 +1,6 @@
+export const PRODUCT = {
+	name: 'Smartwatch',
+	price: '999.99',
+	imageUrl: 'https://robohash.org/1068',
+};
+export const TOAST_CREATE_PRODUCT_SUCCESSFUL = 'Create product successfully';
